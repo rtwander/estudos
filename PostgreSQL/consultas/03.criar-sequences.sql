5591b6234e339f4bcc7d9a6ec33f1cc570f9e7c1
Rust Glomba de Souza <rust.glomba@gmail.com>
2023-07-27 17:51:56 -0300

    adicionado consultas postgresql

---
+++ PostgreSQL/consultas/03.criar-sequences.sql
@@ -0,0 +1,9 @@
+-- Criando sequencias para as tabelas
+--Execute linha a linha
+
+ create sequence mesa_id_seq;
+ create sequence vendas_id_seq;
+ create sequence itens_vendas_id_seq;
+ create sequence produtos_id_seq;
+ create sequence funcionario_id_seq;
+ create sequence comissoes_id_seq;
